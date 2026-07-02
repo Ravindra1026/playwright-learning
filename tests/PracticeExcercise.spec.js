2b69301aafe50df86cf3b3b9293d405d30a453ef
Ravindra1026 <rathoreravindra1026@gmail.com>
2026-07-02 08:13:19 +0530

    2-07-2026 practice update

--- tests/PracticeExcercise.spec.js
+++ tests/PracticeExcercise.spec.js
@@ -8,12 +8,16 @@ test.only('Get all product from List', async({browser})=>
     const email = page.locator('#userEmail');
     const password = page.locator('#userPassword');
     const loginButton = page.locator('#login')
+    const cardTitles = page.locator('.card-body b');
 
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     await email.type("ravindra02@mailinator.com");
     await password.type("Test@123");
     await loginButton.click();
-
+    await page.waitForLoadState('networkidle');
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+    
 
 
 
